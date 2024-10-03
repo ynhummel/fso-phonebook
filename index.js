2480d255d8c0e44bb13a1c8044c96467d86ed0d6
Yuri Hummel <yurihummel@outlook.com>
2024-10-03 18:17:55 -0300

    ex3.4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express();
 
-const persons = [
+let persons = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -44,6 +44,13 @@ app.get("/api/persons/:id", (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter((p) => p.id !== id);
+
+  res.status(204).end();
 });
 
 const PORT = 3001;

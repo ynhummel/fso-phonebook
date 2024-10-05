a4b9e73a7427a7b83528eb8881952e285bdaacbc
Yuri Hummel <yurihummel@outlook.com>
2024-10-05 11:21:17 -0300

    fix port

--- index.js
+++ index.js
@@ -104,5 +104,5 @@ const unknownEndpoint = (req, res) => {
 };
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

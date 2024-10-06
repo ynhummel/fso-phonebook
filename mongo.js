82586cc3d493146b0dc6aac1d01be9baac7c0f98
Yuri Hummel <yurihummel@outlook.com>
2024-10-06 18:11:42 -0300

    ex3.12

---
+++ mongo.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://hummelyn:${password}@fso-ynh.wse8c.mongodb.net/phonebook?retryWrites=true&w=majority&appName=fso-ynh`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length < 4) {
+  Person.find({}).then((result) => {
+    console.log("phonebook:");
+
+    result.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+
+    mongoose.connection.close();
+  });
+} else {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`);
+    mongoose.connection.close();
+  });
+}

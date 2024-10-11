b780f07e18f88c6950f8d94602b1ab33edd1ec43
Yuri Hummel <yurihummel@outlook.com>
2024-10-11 14:50:30 -0300

    ex3.14

--- index.js
+++ index.js
@@ -19,29 +19,6 @@ app.use(
   ),
 );
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/api/persons", (req, res) => {
   Person.find({}).then((people) => res.json(people));
 });
@@ -69,27 +46,18 @@ app.delete("/api/persons/:id", (req, res) => {
 app.post("/api/persons", (req, res) => {
   const body = req.body;
 
-  if (!body.name) {
+  if (body.name === undefined) {
     return res.status(400).json({
       error: "content missing",
     });
   }
 
-  const querryPerson = persons.find((p) => p.name === body.name);
-  if (querryPerson) {
-    return res.status(409).json({
-      error: "name must be unique",
-    });
-  }
-
-  const person = {
-    id: String(Math.floor(Math.random() * (4294967296 - 1) + 1)),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-  res.json(person);
+  person.save().then((saved) => res.json(saved));
 });
 
 const unknownEndpoint = (req, res) => {

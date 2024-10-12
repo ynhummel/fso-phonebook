fba74667867b415095ed0ab781c0aa04bdb9bf8b
Yuri Hummel <yurihummel@outlook.com>
2024-10-12 10:09:51 -0300

    ex3.21 preparation

--- models/person.js
+++ models/person.js
@@ -22,6 +22,7 @@ const personSchema = new mongoose.Schema({
       validator: (v) => {
         return /\d{2,3}-\d{7,8}/.test(v);
       },
+      message: (props) => `${props.value} is not a valid phone number!`,
     },
   },
 });
